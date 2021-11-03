dbd680599b182bf66e360ccd7626d7961eaa2767
Traxiee <danielp2004@msn.com>
2021-11-03 11:03:42 +0100

    Login funktionel

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,38 @@
-function login() {
+import { useState } from "react";
+
+function Login() {
+const [loginUsername, setUsername] = useState("");
+const [loginPassword, setPassword] = useState("");
+
+const onLogin = (e) => {
+  e.preventDefault();
+
+
+
+  const requestLogin = {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ username: loginUsername, password: loginPassword })
+  };
+
+  fetch(process.env.REACT_APP_API_URL + '/user/login', requestLogin)
+    .then(res => {console.log(res)})
+    
+}
+
   return (
-    <div>
-      <p>Login</p>
+    <div id="Login">
+      <div className="login-con">
+          <h1>Login</h1>
+          <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
+          <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+          <button onClick={onLogin}>Login</button>
+      </div>
     </div>
   )
 }
 
-export default login;
+export default Login;
