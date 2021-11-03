22f86e18f291634cae1b39766a9fe3a8f23f2dfc
torbastian <torbastianhansen@hotmail.com>
2021-11-03 12:15:38 +0100

    User Context

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import UserProvider from './hooks/UserContext';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
-        <div id="content">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" component={Login} />
-          </Switch>
+      <UserProvider>
+        <div className="App">
+          <Nav />
+          <div id="content">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </UserProvider>
     </Router>
   );
 }
