49d9714cff786d193c7ad1f011fadbdb76e62e75
torbastian <torbastianhansen@hotmail.com>
2021-11-03 12:24:25 +0100

    Login with userprovider

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,46 @@
 import { useState } from "react";
+import { useUserContext } from "../hooks/UserContext";
 
 function Login() {
-const [loginUsername, setUsername] = useState("");
-const [loginPassword, setPassword] = useState("");
+  const { user, setUser } = useUserContext();
+  const [loginUsername, setUsername] = useState("");
+  const [loginPassword, setPassword] = useState("");
 
-const onLogin = (e) => {
-  e.preventDefault();
+  const onLogin = (e) => {
+    e.preventDefault();
 
+    const requestLogin = {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ username: loginUsername, password: loginPassword })
+    };
 
-
-  const requestLogin = {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({ username: loginUsername, password: loginPassword })
-  };
-
-  fetch(process.env.REACT_APP_API_URL + '/user/login', requestLogin)
-    .then(res => {console.log(res)})
-    
-}
+    fetch(process.env.REACT_APP_API_URL + '/user/login', requestLogin)
+      .then(res => {
+        if (!res.ok) {
+          res.json().then(json => {
+            console.log(json.message);
+          })
+        } else {
+          return res.json().then(user => {
+            setUser(user);
+            //TODO naviger user til boards istedet
+            console.log(user);
+          });
+        }
+      });
+  }
 
   return (
     <div id="Login">
       <div className="login-con">
-          <h1>Login</h1>
-          <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-          <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-          <button onClick={onLogin}>Login</button>
+        <h1>Login</h1>
+        <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={onLogin}>Login</button>
       </div>
     </div>
   )
