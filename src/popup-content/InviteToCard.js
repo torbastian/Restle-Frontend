2d01ae863c5a12511b2f33c41ce539a0fbc14de6
torbastian <torbastianhansen@hotmail.com>
2021-11-19 11:00:38 +0100

    Leave board, polish

--- src/popup-content/InviteToCard.js
+++ src/popup-content/InviteToCard.js
@@ -4,9 +4,14 @@ import MemberSelect from "../components/MemberSelect";
 function InviteToCard(popupData) {
   const [availableMembers, setAvailableMembers] = useState(popupData.availableMembers);
 
+  function doAddMember(members) {
+    popupData.addMember(members);
+    popupData.close();
+  }
+
   return (
     <div className="invite-to-card">
-      <MemberSelect members={availableMembers} addMember={popupData.addMember} />
+      <MemberSelect members={availableMembers} addMember={doAddMember} />
     </div>
   )
 }
